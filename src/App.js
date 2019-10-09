7193bbe523ee15b84a2330e2cb4a64d9794aa26e
Yuranuch <yura477573@mail.ru>
2019-10-09 19:13:09 +0300

    first markup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header/Header";
+import MainContainer from "./Components/MainContainer/MainContainer";
 
 function App() {
   return (
     <div className="App">
-
+        <Header/>
+        <MainContainer/>
     </div>
   );
 }
---
+++ src/Components/MainContainer/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MainContainer.module.css'
+
+function MainContainer() {
+  return (
+    <div className={styles.mainContainer }>
+
+    </div>
+  );
+}
+
+export default MainContainer;
