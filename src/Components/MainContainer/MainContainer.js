c679c52aec82d82e28ccd95291e7d814ffd93d46
Yuranuch <yura477573@mail.ru>
2019-10-20 17:26:05 +0300

    add new fonts

--- src/Components/MainContainer/MainContainer.js
+++ src/Components/MainContainer/MainContainer.js
@@ -7,9 +7,9 @@ function MainContainer() {
         <div className={styles.mainContainer}>
             <div className={styles.container}>
                 <div className={styles.greeting}>
-                    <div>Hello</div>
-                    <div className={styles.fullName}>My name is Tsikhanovich Yury</div>
-                    <div>I'm FrontEnd Developer</div>
+                    <div className={styles.greetHi}>Hello</div>
+                    <div className={styles.fullName}>My name is <span>Tsikhanovich Yury</span></div>
+                    <div className={styles.prof}>I'm FrontEnd Developer</div>
                 </div>
                 <div className={styles.photo}>
                     <img src={photo} alt=""/>
