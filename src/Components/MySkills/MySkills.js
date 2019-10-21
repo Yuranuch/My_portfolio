894bcf86f2e3dfc348d6c7c4b970adf70d61c857
Yuranuch <yura477573@mail.ru>
2019-10-21 10:59:08 +0300

    added logos

--- src/Components/MySkills/MySkills.js
+++ src/Components/MySkills/MySkills.js
@@ -2,18 +2,29 @@ import React from 'react';
 import styles from './MySkills.module.css'
 import SkillItem from "./SkillItem/SkillItem";
 import Title from "../../reuseComponents/Title/Title";
+import reactLogo from "./../../assets/images/react.png"
+import jsLogo from "./../../assets/images/Js.png"
+import htmlLogo from "./../../assets/images/HTML&CSS.png"
+
 
 function MySkills(props) {
+
     return (
         <div className={styles.mySkills}>
-
             <div className={styles.container}>
                 <Title title="My Skills"/>
                 <div className={styles.flexContainer}>
-                    <SkillItem title="HTML&CSS" info="Lorem oiiwvowi Lorem sdlkvds skmre psdkvs lsdlkvds skmre Lorem sdlkvds skmre psdkvs psdkvsv Lorem sdlkvds skmre psdkvs pdspvrei psivdmv pmvpmfv"/>
-                    <SkillItem title="Js" info="Lorem oiiwvowi lsdlpsivdmv pmvpmfv Lorem Lorem sdlkvds skmre psdkvs sdlkvds skmre psdkvs"/>
-                    <SkillItem title="React" info="Lorem sdlkvds skmre psdkvsv Lorem Lorem sdlkvds skmre psdkvs sdlkvds skmre psdkvs pdspvrei psivdmv pmvpmfv"/>
+                    <SkillItem name={htmlLogo}
+                               title="HTML&CSS"
+                               info="Lorem oiiwvowi Lorem sdlkvds skmre psdkvs lsdlkvds skmre Lorem sdlkvds skmre psdkvs psdkvsv Lorem sdlkvds skmre psdkvs pdspvrei psivdmv pmvpmfv"/>
+                    <SkillItem name={jsLogo}
+                               title="Js"
+                               info="Lorem oiiwvowi lsdlpsivdmv pmvpmfv Lorem Lorem sdlkvds skmre psdkvs sdlkvds skmre psdkvs"/>
+                    <SkillItem name={reactLogo}
+                               title="React"
+                               info="Lorem sdlkvds skmre psdkvsv Lorem Lorem sdlkvds skmre psdkvs sdlkvds skmre psdkvs pdspvrei psivdmv pmvpmfv"/>
                 </div>
+
             </div>
         </div>
     );
--- src/Components/MySkills/SkillItem/SkillItem.js
+++ src/Components/MySkills/SkillItem/SkillItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styles from './SkillItem.module.css'
 
+
+
 function SkillItem(props) {
     return (
         <div className={styles.skillItem}>
             <div className={styles.imageBlock}>
+                <img src={props.name} alt=''/>
             </div>
             <span className={styles.skillTitle}>{props.title}</span>
             <div className={styles.skillInfo}>
                 <p>{props.info}</p>
             </div>
+
         </div>
 
     );
