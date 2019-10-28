e353403281e5cc3dc7fbd9a2e793535e29345340
Yuranuch <yura477573@mail.ru>
2019-10-28 11:22:15 +0300

    Updates

---
+++ precache-manifest.e75502c665e2d626549cc71862c9c6e5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f91d0b3a11fa52db2e1b0c1e6795671",
+    "url": "/My_portfolio/index.html"
+  },
+  {
+    "revision": "23840615fffc5ad16ae2",
+    "url": "/My_portfolio/static/css/main.1fae5d8c.chunk.css"
+  },
+  {
+    "revision": "953fac87c8d5271729fb",
+    "url": "/My_portfolio/static/js/2.a1c86453.chunk.js"
+  },
+  {
+    "revision": "23840615fffc5ad16ae2",
+    "url": "/My_portfolio/static/js/main.46c03648.chunk.js"
+  },
+  {
+    "revision": "3b48f81dc3b9df8f844e",
+    "url": "/My_portfolio/static/js/runtime-main.ff3d3e7a.js"
+  },
+  {
+    "revision": "1b9dc5308f625c8092637ae186bbc73f",
+    "url": "/My_portfolio/static/media/014.1b9dc530.jpg"
+  },
+  {
+    "revision": "3cff1dde8cb69d8c208c1fb768f75e0a",
+    "url": "/My_portfolio/static/media/Bricks-Texture-Black-Background.3cff1dde.jpg"
+  },
+  {
+    "revision": "913b527bab01c106d04cb160a278a25a",
+    "url": "/My_portfolio/static/media/HTML&CSS.913b527b.png"
+  },
+  {
+    "revision": "cc58becb35681079d3742dade6f27c0b",
+    "url": "/My_portfolio/static/media/Js.cc58becb.png"
+  },
+  {
+    "revision": "8e88e0d7ba0b8fe35dbafce11c1f4828",
+    "url": "/My_portfolio/static/media/project_1.8e88e0d7.jpg"
+  },
+  {
+    "revision": "e6df1fa56f5095ef77ffcea2b48759d8",
+    "url": "/My_portfolio/static/media/project_2.e6df1fa5.jpg"
+  },
+  {
+    "revision": "f950e8ddcaf00269dc109dd2829f8e74",
+    "url": "/My_portfolio/static/media/project_3.f950e8dd.jpg"
+  },
+  {
+    "revision": "45c5dfbdae012b19837bdcba4c9a9054",
+    "url": "/My_portfolio/static/media/project_4.45c5dfbd.jpg"
+  },
+  {
+    "revision": "d2a23832ac959cc7bb01372b216459fd",
+    "url": "/My_portfolio/static/media/project_5.d2a23832.jpg"
+  },
+  {
+    "revision": "2fb0360953fca136f43bef7934e4ad07",
+    "url": "/My_portfolio/static/media/project_6.2fb03609.jpg"
+  },
+  {
+    "revision": "5c1edff00fe0ed6afe3a31b02448a5d4",
+    "url": "/My_portfolio/static/media/react.5c1edff0.png"
+  }
+]);
